970ca88adfb5c81c88e7027d235b7d31d27cbd5b
mrugendra <mrugendra.shilvant@techuplabs.com>
2024-07-16 22:11:56 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { SkillComponent } from './skill/skill.component';
-import { SkillDescriptionComponent } from './skill/skill-description/skill-description.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from "@angular/material/menu";
 import {CdkMenu, CdkMenuBar, CdkMenuItem, CdkMenuTrigger} from "@angular/cdk/menu";
@@ -12,9 +8,6 @@ import {CdkMenu, CdkMenuBar, CdkMenuItem, CdkMenuTrigger} from "@angular/cdk/men
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    SkillComponent,
-    SkillDescriptionComponent
   ],
   imports: [
     BrowserModule,
