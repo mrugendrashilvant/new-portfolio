9de888dd00868465e360d93b5e50fca0d61002e0
Mrugendra Shilvant <mrugendra.shilvant@techuplabs.com>
2024-10-08 14:09:52 +0530

    adding experience

--- src/app/sections/experience/experience.component.ts
+++ src/app/sections/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-experience',
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements AfterViewInit{
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event:any) {
+    console.log("scrolling")
+  }
+  // onScroll(event: Event) {
+  //   console.log("scrolling")
+  //   if(this.isInViewport()) {
+  //     this.moveMarker();
+  //   }
+  // }
+  @ViewChild('marker')marker!:ElementRef<HTMLDivElement>;
+  @ViewChild('path') path!:ElementRef<HTMLImageElement>;
 
+  ngAfterViewInit() {
+  }
+
+  moveMarker() {
+    this.marker.nativeElement.classList.add('move');
+  }
+
+  isInViewport() {
+    const rect = this.path.nativeElement.getBoundingClientRect();
+    console.log(rect);
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
 }
